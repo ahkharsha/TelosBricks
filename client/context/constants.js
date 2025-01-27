849c88bc54af3f257b9156eadab9b55b543d4d5f
ahkharsha <ahkharsha@gmail.com>
2025-01-27 20:02:35 +0530

    Frontend

---
+++ client/context/constants.js
@@ -0,0 +1,69 @@
+import realEstate from "./RealEstate.json";
+require("dotenv").config();
+
+export const REAL_ESTATE_ADDRESS = "0xF859e215A276EC632Fb46DCca4EbccF99C8993D2";
+export const REAL_ESTATE_ABI = realEstate.abi;
+
+export const PINATA_API_KEY = "da212708ca61ceb1b91d";
+export const PINATA_SECRET_KEY = "ce9bea71753244b0e67a0b3039e09889b69d15b863d25f3b9110449afae55c05";
+
+// NETWORK
+const networks = {
+  telos_mainnet: {
+    chainId: `0x${Number(40).toString(16)}`,
+    chainName: "Telos Mainnet",
+    nativeCurrency: {
+      name: "TLOS",
+      symbol: "TLOS",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc.telos.net"],
+    blockExplorerUrls: ["https://www.teloscan.io/"],
+  },
+  telos_testnet: {
+    chainId: `0x${Number(41).toString(16)}`,
+    chainName: "Telos Testnet",
+    nativeCurrency: {
+      name: "TLOS",
+      symbol: "TLOS",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc.testnet.telos.net"],
+    blockExplorerUrls: ["https://testnet.teloscan.io/"],
+  },
+  localhost: {
+    chainId: `0x${Number(5003).toString(16)}`,
+    chainName: "localhost",
+    nativeCurrency: {
+      name: "TLOS",
+      symbol: "TLOS",
+      decimals: 18,
+    },
+    rpcUrls: ["http://127.0.0.1:8545/"],
+    blockExplorerUrls: ["https://testnet.teloscan.io/"],
+  },
+};
+
+const changeNetwork = async ({ networkName }) => {
+  try {
+    if (!window.ethereum) throw new Error("No crypto wallet found");
+    await window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          ...networks[networkName],
+        },
+      ],
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const ACTIVE_NETWORK = "telos_testnet";
+
+export const handleNetworkSwitch = async () => {
+  const networkName = "telos_testnet";
+  const network = await changeNetwork({ networkName });
+  return networkName;
+};
